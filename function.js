bf0a678e65a85775efbedc700aacd9371c26e9f6
Memet <meavci@gmail.com>
2024-11-29 11:58:23 -0600

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class function extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>function</div>
+    )
+  }
+}
